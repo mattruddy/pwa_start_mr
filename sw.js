be29b0e16a1655ad8b3015b333bb49cc01daad6d
stcentor <skyler.tangneycentor@gmail.com>
2019-12-12 09:09:09 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/tones',
+  '/comments',
+  'app.js',
+  'manifest.json',
+  'views/index.ejs',
+  'views/tones.ejs',
+  'views/comments.ejs',
+  'css/style.css',
+  'main.js'
+];
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+    console.log("installing");
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
